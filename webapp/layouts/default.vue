9f68b8b4d46a1b0891cc72ca536d06e1e51f5c31
Jan <55839828+Piitschy@users.noreply.github.com>
2023-02-03 20:49:02 +0100

    format

--- webapp/layouts/default.vue
+++ webapp/layouts/default.vue
@@ -1,30 +1,30 @@
 <template>
   <v-container>
-        <v-row>
-          <v-col
-            cols="12"
-            sm="2"
-          >
-          </v-col>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="2"
+      >
+      </v-col>
 
-          <v-col
-            cols="12"
-            sm="8"
-          >
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-              class="px-5 py-5"
-            >
-              <slot />
-            </v-sheet>
-          </v-col>
+      <v-col
+        cols="12"
+        sm="8"
+      >
+        <v-sheet
+          min-height="70vh"
+          rounded="lg"
+          class="px-5 py-5"
+        >
+          <slot />
+        </v-sheet>
+      </v-col>
 
-          <v-col
-            cols="12"
-            sm="2"
-          >
-          </v-col>
-        </v-row>
-      </v-container>
+      <v-col
+        cols="12"
+        sm="2"
+      >
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
