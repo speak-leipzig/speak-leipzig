db99d51b319dd7605288fce648dfdad8b7cdd8f4
Jan <55839828+Piitschy@users.noreply.github.com>
2023-02-06 00:17:28 +0100

    фіх ру бтн

---
+++ webapp/locales/uiElements.ts
@@ -0,0 +1,78 @@
+export default {
+  en: {
+    button: {
+      save: 'Save',
+      cancel: 'Cancel',
+      delete: 'Delete',
+      edit: 'Edit',
+      add: 'Add',
+      close: 'Close',
+      yes: 'Yes',
+      no: 'No',
+      ok: 'Ok',
+      back: 'Back',
+      next: 'Next',
+      finish: 'Finish',
+      submit: 'Submit',
+      search: 'Search',
+      clear: 'Clear' 
+    }
+  },
+  de: {
+    button: {
+      save: 'Speichern',
+      cancel: 'Abbrechen',
+      delete: 'Löschen',
+      edit: 'Bearbeiten',
+      add: 'Hinzufügen',
+      close: 'Schließen',
+      yes: 'Ja',
+      no: 'Nein',
+      ok: 'Ok',
+      back: 'Zurück',
+      next: 'Weiter',
+      finish: 'Fertigstellen',
+      submit: 'Absenden',
+      search: 'Suchen',
+      clear: 'Löschen'
+    }
+  },
+  ua: {
+    button: {
+      save: 'Зберегти',
+      cancel: 'Скасувати',
+      delete: 'Видалити',
+      edit: 'Редагувати',
+      add: 'Додати',
+      close: 'Закрити',
+      yes: 'Так',
+      no: 'Ні',
+      ok: 'Ok',
+      back: 'Назад',
+      next: 'Далі',
+      finish: 'Завершити',
+      submit: 'Відправити',
+      search: 'Пошук',
+      clear: 'Очистити'
+    }
+  },
+  ru: {
+    button: {
+      save: 'Сохранить',
+      cancel: 'Отменить',
+      delete: 'Удалить',
+      edit: 'Редактировать',
+      add: 'Добавить',
+      close: 'Закрыть',
+      yes: 'Да',
+      no: 'Нет',
+      ok: 'Ok',
+      back: 'Назад',
+      next: 'Далее',
+      finish: 'Завершить',
+      submit: 'Отправить',
+      search: 'Поиск',
+      clear: 'Очистить'
+    }
+  }
+}
