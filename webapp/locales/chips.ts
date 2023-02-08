228f225471c6811aac7b4035684a9dad9c1d3d14
Jan <55839828+Piitschy@users.noreply.github.com>
2023-02-08 11:13:51 +0100

    chip translation

---
+++ webapp/locales/chips.ts
@@ -0,0 +1,18 @@
+export default {
+  en: {
+    online: 'online',
+    presence: 'presence',
+  },
+  de: {
+    online: 'online',
+    presence: 'Präsenz',
+  },
+  ua: {
+    online: 'онлайн',
+    presence: 'присутність',
+  },
+  ru: {
+    online: 'онлайн',
+    presence: 'присутствие',
+  }
+}
