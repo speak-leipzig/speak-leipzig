9a5c29f3e7ef56b9e2a6388a78d50de853b42246
Jan <55839828+Piitschy@users.noreply.github.com>
2023-02-10 00:28:55 +0100

    start with typing

---
+++ webapp/types/directus.d.ts
@@ -0,0 +1,9 @@
+export interface Member {
+  first_name: string;
+  last_name:  string;
+  email:      string;
+  avatar:     string;
+  title:      string;
+  id:         string;
+  languages:  number[];
+}
