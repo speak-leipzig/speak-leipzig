b0785123d6fd8d409bc2543a18bac3ce24819b36
Jan <55839828+Piitschy@users.noreply.github.com>
2023-02-10 00:30:02 +0100

    translate langs

--- webapp/locales/.merge.ts
+++ webapp/locales/.merge.ts
@@ -1,3 +1,4 @@
+import languages from './languages'
 import menu from './menu'
 import level from './level'
 import chips from './chips'
@@ -7,8 +8,8 @@ import uiElements from './uiElements'
 
 // Merge all the locales into one object
 export default {
-  en: { ...menu.en, ...level.en, ...chips.en, ...uiElements.en },
-  de: { ...menu.de, ...level.de, ...chips.de, ...uiElements.de },
-  ua: { ...menu.ua, ...level.ua, ...chips.ua, ...uiElements.ua },
-  ru: { ...menu.ru, ...level.ru, ...chips.ru, ...uiElements.ru }
+  en: { ...languages.en, ...menu.en, ...level.en, ...chips.en, ...uiElements.en },
+  de: { ...languages.de, ...menu.de, ...level.de, ...chips.de, ...uiElements.de },
+  ua: { ...languages.ua, ...menu.ua, ...level.ua, ...chips.ua, ...uiElements.ua },
+  ru: { ...languages.ru, ...menu.ru, ...level.ru, ...chips.ru, ...uiElements.ru }
 }
---
+++ webapp/locales/languages.ts
@@ -0,0 +1,26 @@
+export default {
+  en: {
+    en: 'English',
+    de: 'German',
+    ua: 'Ukrainian',
+    ru: 'Russian',
+  },
+  de: {
+    en: 'Englisch',
+    de: 'Deutsch',
+    ua: 'Ukrainisch',
+    ru: 'Russisch',
+  },
+  ua: {
+    en: 'Англійська',
+    de: 'Німецька',
+    ua: 'Українська',
+    ru: 'Російська',
+  },
+  ru: {
+    en: 'Английский',
+    de: 'Немецкий',
+    ua: 'Украинский',
+    ru: 'Русский',
+  }
+}
