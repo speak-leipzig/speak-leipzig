7099222cf87c15725d656c72772c29984ae65232
Jan <55839828+Piitschy@users.noreply.github.com>
2023-02-10 00:29:34 +0100

    dynamic team page

---
+++ webapp/components/team/card.vue
@@ -0,0 +1,70 @@
+<script setup>
+const router = useRouter()
+const { locale } = useI18n()
+const { getItems } = useDirectusItems()
+const { getThumbnail: img } = useDirectusFiles()
+
+const props = defineProps({
+  value: {
+    type: Object,
+    required: true
+  }
+})
+
+const member = $computed(() => props.value)
+
+const descriptions = await getItems({
+  collection: 'junction_directus_users_translations',
+  fields: ['languages_code', 'public_description'],
+  filter: {
+    directus_users_id: {
+      _eq: member.id
+    }
+  }
+})
+
+const description = $computed(() => descriptions.find((description) => description.languages_code === locale.value).public_description)
+</script>
+
+<template>
+  <v-card class="card mx-auto" max-width="95%" width="800px" @click="router.push(localePath(`/team/vita`))" >
+    <div class="d-flex justify-space-between" >
+      <div>
+        <v-card-title class="mt-2">
+          {{ member.first_name }} {{ member.last_name }}
+        </v-card-title>
+        <v-card-text>
+          {{ description }}
+        </v-card-text>
+        <v-list style="background: rgba(0,0,0,0)!important;">
+          <v-list-item>
+            <v-list-item-title>
+              <v-icon class="mr-4">mdi-translate</v-icon>
+              <WrapperChip :tooltip="$t('de')">Deutsch</WrapperChip>
+              <WrapperChip :tooltip="$t('en')">Englisch</WrapperChip>
+              <WrapperChip :tooltip="$t('ua')">українська</WrapperChip>
+              <WrapperChip :tooltip="$t('ru')">русский</WrapperChip>
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>
+              <span class="mr-7">
+                <v-icon class="mr-4">mdi-email-fast-outline</v-icon>
+                <a :href="`mailto:${member.email}`">{{ member.email }}</a>
+              </span>
+              <span>
+                <v-icon class="mr-4">mdi-message-fast-outline</v-icon>
+                <a :href="``">Telegram</a>
+              </span>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </div>
+      <v-avatar
+        class="ma-4"
+        :image="img(member.avatar, { width: 128, height: 128, fit: 'cover' })"
+        size="128"
+      ></v-avatar>
+    </div>
+  </v-card>
+</template>
--- webapp/pages/team/index.vue
+++ webapp/pages/team/index.vue
@@ -1,5 +1,15 @@
 <script setup>
-//TODO: useDirectusUsers() to get all users
+//! Here is a Bug in the npm package "nuxt/directus" for the useDirectusUsers() function.
+// const { getUsers } = useDirectusUsers();
+import {useCmsUrl} from '../../composables/states';
+
+const cmsUrl = useCmsUrl()
+
+const members = await fetch(`${cmsUrl.value}/users`)
+  .then((res) => res.json())
+  .then((res) => res.data)
+  .catch((err) => console.error(err))
+
 </script>
 
 <template>
@@ -8,7 +18,9 @@
       <h1 v-html="translation.title"></h1>
       <span v-html="translation.content"></span>
     </WrapperTranslation>
-    <br>
-    <TeamMember />
+    <div class="my-10"></div>
+    <v-row class="my-5" v-for="member in members" :key="member.id">
+      <TeamCard :value="member"/>
+    </v-row>
   </NuxtLayout>
 </template>
