9658bce8a720f93f4a591845a5f37d615ca4c42c
Jan <55839828+Piitschy@users.noreply.github.com>
2023-03-03 16:26:00 +0100

    set flex-warp

--- webapp/components/offer/card.vue
+++ webapp/components/offer/card.vue
@@ -10,6 +10,9 @@ const props = defineProps({
     required: false
   }
 })
+const overflow = (str, limit) => {
+  return str.slice(0, limit) + (str.length > limit ? '...' : '')
+}
 </script>
 
 
@@ -21,28 +24,28 @@ const props = defineProps({
     @click="router.push(localePath(`/offers/${offer.id}`))"
   >
     <WrapperTranslation v-slot="{ translation }" collection="offers" :id="offer.id">
-      <v-card-title class="d-flex justify-space-between">
-        <span v-html="translation.title" class="headline"></span>
+      <v-card-title class="d-flex flex-wrap justify-space-between">
+        <span v-html="overflow(translation.title, 35)" class="headline"></span>
         <v-spacer></v-spacer>
-        <div v-if="facility">
-          <v-chip
-            color="grey-darken-1 mx-2"
-            text-color="white"
-            label
-          >
-              {{ facility.short_name }}
-                <v-tooltip 
-                  activator="parent"
-                  location="bottom"
-                >
-                {{ facility.name }}
-                </v-tooltip>
-          </v-chip>
-          <v-avatar
-            :image="img(facility.logo, 'fit', 32, 32)"
-            size="32"
-          ></v-avatar>
-        </div>
+          <div v-if="facility" style="margin-left: auto;">
+            <v-chip
+              color="grey-darken-1 mx-2"
+              text-color="white"
+              label
+            >
+                {{ facility.short_name }}
+                  <v-tooltip 
+                    activator="parent"
+                    location="bottom"
+                  >
+                  {{ facility.name }}
+                  </v-tooltip>
+            </v-chip>
+            <v-avatar
+              :image="img(facility.logo, 'fit', 32, 32)"
+              size="32"
+            ></v-avatar>
+          </div>
       </v-card-title>
       <v-card-subtitle class="d-flex justify-start">
         <div v-for="lvl in offer.level" :key="lvl">
