3681be097d0811c809293f86a2534efd2478759e
Jan <55839828+Piitschy@users.noreply.github.com>
2023-03-15 13:39:33 +0100

    add target="_blank" to mail links

--- webapp/pages/team/[id].vue
+++ webapp/pages/team/[id].vue
@@ -52,7 +52,7 @@ const translation = $computed(() => translations.find((translation) => translati
                 <v-icon icon="mdi-email-fast-outline"></v-icon>
               </template>
               <v-list-item-title>
-                <a :href="`mailto:${user.email}`">{{ user.email }}</a>
+                <a :href="`mailto:${user.email}`" target="_blank">{{ user.email }}</a>
               </v-list-item-title>
             </v-list-item>
             <v-list-item>
@@ -60,7 +60,7 @@ const translation = $computed(() => translations.find((translation) => translati
                 <v-icon icon="mdi-message-fast-outline"></v-icon>
               </template>
               <v-list-item-title>
-                <a :href="``">Telegram</a>
+                <a :href="``" target="_blank">Telegram</a>
               </v-list-item-title>
             </v-list-item>
           </WrapperMinimize>
