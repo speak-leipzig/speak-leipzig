5f1edc907ece2375f048c93d752bf278a3286ed2
Jan <55839828+Piitschy@users.noreply.github.com>
2023-03-20 00:54:52 +0100

    start with geocoding

--- extensions/geocoding/src/index.ts
+++ extensions/geocoding/src/index.ts
@@ -1,11 +1,38 @@
 import { defineHook } from '@directus/extensions-sdk';
 
-export default defineHook(({ filter, action }) => {
-	filter('items.create', () => {
-		console.log('Creating Item!');
-	});
+interface Adress {
+	street: string;
+	no: string;
+	city: string;
+	zip: string;
+}
 
-	action('items.create', () => {
-		console.log('Item wurde erstellt!');
-	});
+interface GeoJSON {
+	type: 'Point';
+	coordinates: [number, number];
+}
+
+function geocode(adress : Adress):GeoJSON {
+	// Geocode the adress and return the coordinates
+	return {
+		type: 'Point',
+		coordinates: [9.722082076194539, 52.384651362832585]
+	};
+}
+
+export default defineHook(({ filter }) => {
+	filter('items.create', async (item) => {
+		console.log(item);
+		if (!('street' in item && 'city' in item && 'zip' in item && 'no' in item)) return item;
+
+		const address: Adress = {
+			street: item.street,
+			no: item.no,
+			city: item.city,
+			zip: item.zip,
+		};
+
+		item.geojson = geocode(address);
+		return item;
+	})
 });
--- extensions/greet/src/index.js
+++ extensions/greet/src/index.js
@@ -1,7 +1,7 @@
 export default {
 	id: 'greet',
 	handler: (router) => {
-		router.get('/', (req, res) => res.send('He11o, Wor1d!'));
+		router.get('/', (req, res) => res.send('He11o, World!'));
 		router.get('/intro', (req, res) => res.send('Nice to meet you.'));
 		router.get('/goodbye', (req, res) => res.send('Goodbye!'));
 	},
