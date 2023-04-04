73300fa1f88b301d3baf7ed016628f4b42911bab
Jan <55839828+Piitschy@users.noreply.github.com>
2023-04-04 22:17:58 +0200

    capsle OffrChips

--- webapp/components/offer/card.vue
+++ webapp/components/offer/card.vue
@@ -48,20 +48,8 @@ const overflow = (str, limit) => {
           ></v-avatar>
         </div>
       </v-card-title>
-      <v-card-subtitle class="d-flex justify-start">
-        <div v-for="lvl in offer.level" :key="lvl">
-          <WrapperChip :tooltip="`${$t('level')}: ${$t(lvl)}`">
-            {{ $t(lvl) }}
-          </WrapperChip>
-        </div>
-
-        <WrapperChip v-if="!offer.online" :tooltip="$t('presence')">
-          <v-icon>mdi-account-group</v-icon>
-        </WrapperChip>
-        <WrapperChip v-else :tooltip="$t('online')">
-          <v-icon>mdi-web</v-icon>
-        </WrapperChip>
-        
+      <v-card-subtitle>
+        <OfferChips :offer="offer" />        
       </v-card-subtitle>
     <v-card-text v-html="translation.short_description"></v-card-text>
   </WrapperTranslation>
