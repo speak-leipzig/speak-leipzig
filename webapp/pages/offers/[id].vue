ca63072b0f23a1e1aaa8cfd5b15c28af97d1405c
Jan <55839828+Piitschy@users.noreply.github.com>
2023-04-16 19:15:23 +0200

    delete week_2&3

--- webapp/pages/offers/[id].vue
+++ webapp/pages/offers/[id].vue
@@ -76,20 +76,20 @@ function gMapsLink() {
                   </v-list-item-title>
                 </v-list-item>
               </WrapperTranslation>
-              <div v-if="offer.weekday">
+              <div v-if="offer.weekday && offer.show_weekday">
                 <v-list-item class="my-2">
                   <v-list-item-title>
                     <v-icon v-if="lgAndUp || mobile">mdi-calendar</v-icon>
                       {{ $t(offer.weekday) }}
                     <br>
-                    <span v-if="offer.time_start">
+                    <span v-if="offer.time_start && offer.show_time">
                       <v-icon v-if="lgAndUp || mobile">mdi-clock</v-icon>
                       {{ offer.time_start?.substring(0,5) }} <span v-if="offer.time_end">- {{ offer.time_end?.substring(0,5) }}</span>
                     </span>
                   </v-list-item-title>
                 </v-list-item>
               </div>
-              <v-list-item v-if="offer.contact_show">
+              <v-list-item v-if="offer.contact_show" class="mt-3">
                 <v-list-item-title class="mb-n2">
                   {{ $t('contact') }}
                 </v-list-item-title>
@@ -109,34 +109,6 @@ function gMapsLink() {
                   <a :href="`mailto:${offer.contact_mail}`" target="_blank"> {{ offer.contact_mail }}</a>
                 </div>
               </v-list-item>
-              <!--start week 2 and 3 -->
-              <div v-if="offer.weekday_2">
-                <v-list-item class="my-2">
-                  <v-list-item-title>
-                    <v-icon v-if="lgAndUp || mobile">mdi-calendar</v-icon>
-                      {{ $t(offer.weekday_2) }}
-                      <br>
-                      <span v-if="offer.time_start_2">
-                        <v-icon v-if="lgAndUp || mobile">mdi-clock</v-icon>
-                        {{ offer.time_start_2?.substring(0,5) }} <span v-if="offer.time_end_2">- {{ offer.time_end_2?.substring(0,5) }}</span>
-                      </span>
-                  </v-list-item-title>
-                </v-list-item>
-              </div>
-              <div v-if="offer.weekday_3">
-                <v-list-item class="my-2">
-                  <v-list-item-title>
-                    <v-icon v-if="lgAndUp || mobile">mdi-calendar</v-icon>
-                      {{ $t(offer.weekday_3) }}
-                      <br>
-                      <span v-if="offer.time_start_3">
-                        <v-icon v-if="lgAndUp || mobile">mdi-clock</v-icon>
-                        {{ offer.time_start_3?.substring(0,5) }} <span v-if="offer.time_end_3">- {{ offer.time_end_3?.substring(0,5) }}</span>
-                      </span>
-                  </v-list-item-title>
-                </v-list-item>
-              </div>
-              <!--end week 2 and 3 -->
             </v-list>
           </v-card-text>
         </v-card>
